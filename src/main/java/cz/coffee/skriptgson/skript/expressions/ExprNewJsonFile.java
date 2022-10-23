def5c2077d49070c80ef087aca572e0c07728128
cooffeeRequired <106232282+cooffeeRequired@users.noreply.github.com>
2022-10-23 11:02:18 +0200

    ExprNewJsonFile..
    ```vb
    set {-y} to new json file "Test/json.json"
    set {-y} to new json file "Test/json.json" with "{'Some': true}"
    
    #/ Output
    Test/json.json
    ```

--- src/main/java/cz/coffee/skriptgson/skript/expressions/ExprNewJsonFile.java
+++ src/main/java/cz/coffee/skriptgson/skript/expressions/ExprNewJsonFile.java
@@ -9,64 +9,100 @@ import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.stream.JsonWriter;
 import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
 @Name("New json File")
 @Description("Creates a new JSON file")
 @Since("1.0")
 
 
-@SuppressWarnings("unchecked")
+@SuppressWarnings({"unchecked","unused"})
 public class ExprNewJsonFile extends SimpleExpression<Object> {
 
     static {
         Skript.registerExpression(ExprNewJsonFile.class,Object.class, ExpressionType.COMBINED,"" +
                 "[a] [new] json file %string%",
-                "[a] [new] json file %string% with [data] %object%"
-        );
+                "[a] [new] json file %string% with [data] %string%");
 
     }
 
     private Expression<String> exprString;
-    private Expression<Object> exprObject;
+    private Expression<String> exprData;
     private int pattern;
 
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
         exprString = (Expression<String>) exprs[0];
-        exprObject = (Expression<Object>) exprs[1];
         pattern = matchedPattern;
+        if ( pattern == 1) {
+            exprData = (Expression<String>) exprs[1];
+        }
         return true;
     }
 
-    public File[] get(Event event) {
-        if ( pattern == 0) {
-            try {
-                JsonElement json = JsonParser.parseString("");
-            } finally {
-                JsonElement json = JsonParser.parseString("");
-            }
+    public Object[] get(@NotNull Event event) {
+        if (exprString == null)
+            return null;
+        String inputFile = exprString.getSingle(event);
+        if (inputFile == null)
+            return null;
+
+        FileOutputStream OutputStream;
+        JsonWriter writer;
+        JsonElement parsedString;
+        String inputData;
+
+        if (exprData != null) {
+            inputData = exprData.getSingle(event);
+        } else {
+            inputData = null;
+        }
+
+        if (new File(inputFile).exists()) {
             return null;
         }
-        return new File[0];
+
+        try {
+            OutputStream = new FileOutputStream(inputFile);
+            writer = new JsonWriter(new OutputStreamWriter(OutputStream, StandardCharsets.UTF_8));
+            writer.setIndent("    ");
+            writer.jsonValue(new GsonBuilder()
+                    .setPrettyPrinting()
+                    .create()
+                    .toJson(
+                            JsonParser.parseString(pattern == 0 ? "{}" : inputData)
+                    )
+            );
+            writer.flush();
+            writer.close();
+        } catch (IOException | JsonSyntaxException e) {
+            return null;
+        }
+        return new Object[]{inputFile};
     }
 
     @Override
-    public String toString(Event event,boolean debug) {
-        return "json file " + (pattern == 0 ? "" : "with data");
-    }
+    public String toString(Event event,boolean debug) {return "json file " + (pattern == 0 ? "" : "with data");}
 
     @Override
     public boolean isSingle() {
-        return false;
+        return true;
     }
 
     @Override
     public Class<?> getReturnType() {
-        return null;
+        return Object.class;
     }
 }
