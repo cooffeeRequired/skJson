72d44eb17dc19d1616a2329356c682f13e5f44eb
cooffeeRequired <106232282+cooffeeRequired@users.noreply.github.com>
2022-11-01 17:24:05 +0100

    Version Think
    From Literal version string to Variable from Gradle

--- src/main/java/cz/coffee/skriptgson/SkriptGson.java
+++ src/main/java/cz/coffee/skriptgson/SkriptGson.java
@@ -21,8 +21,7 @@ public class SkriptGson extends JavaPlugin {
     private static Logger logger;
 
     private static SkriptGson instance;
-    public static final String PREFIX = "&7[&l&bskript-gson&7] ";
-    public static final String docSince = "1.0";
+    public static final String PREFIX = "&7[&l&eskript-gson&7] ";
     private SkriptAddon addon;
     private final PluginManager pluginManager = this.getServer().getPluginManager();
 
