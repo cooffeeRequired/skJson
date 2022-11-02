55ba3b2b39a305abb1d3a5fab3f2918dc87467bb
cooffeeRequired <106232282+cooffeeRequired@users.noreply.github.com>
2022-11-02 15:37:07 +0100

    ** Feature -> Prepared, after rework newJsonFile API

---
+++ src/main/java/cz/coffee/skriptgson/skript/effect/EffCloseOpenedFile.java
@@ -0,0 +1,8 @@
+package cz.coffee.skriptgson.skript.effect;
+/**
+ * ! will be used for new structure of ExprNewJsonFile
+ * ? currently without any change
+ * TODO Rework the ExprNewJsonFile, will return the opened FileWriter.
+ */
+public class EffCloseOpenedFile {
+}
