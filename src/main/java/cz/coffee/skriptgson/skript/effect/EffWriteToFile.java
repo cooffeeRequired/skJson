8e6d30ee8c8df9c26dffe1130704b5881940bbb3
cooffeeRequired <106232282+cooffeeRequired@users.noreply.github.com>
2022-11-02 20:06:00 +0100

    Fixed 'as key %string%'

--- src/main/java/cz/coffee/skriptgson/skript/effect/EffWriteToFile.java
+++ src/main/java/cz/coffee/skriptgson/skript/effect/EffWriteToFile.java
@@ -2,7 +2,6 @@ package cz.coffee.skriptgson.skript.effect;
 
 
 import ch.njol.skript.Skript;
-import ch.njol.skript.SkriptAPIException;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
@@ -10,17 +9,20 @@ import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.util.LiteralUtils;
 import ch.njol.util.Kleenean;
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
-import cz.coffee.skriptgson.SkriptGson;
 import org.bukkit.event.Event;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 
@@ -43,7 +45,7 @@ public class EffWriteToFile extends Effect {
 
     static {
         Skript.registerEffect(EffWriteToFile.class,
-                "(:write|:put) data %jsonelement% to json file %object% [(:as key) %-string%]"
+                "(:write|:put) data %jsonelement% to json file %object% [(:as) key %-string%]"
         );
     }
 
@@ -59,10 +61,8 @@ public class EffWriteToFile extends Effect {
             return;
 
         JsonElement json = jsonelement.getSingle(e);
-        File file;
-        try {
-            file = object.getSingle(e);
-        } catch (SkriptAPIException ex) {ex.printStackTrace();return;}
+        String key = k ? inKey.getSingle(e) : "";
+        File file = object.getSingle(e);
 
         FileOutputStream outStream;
         JsonWriter jsonWriter;
@@ -72,9 +72,9 @@ public class EffWriteToFile extends Effect {
                 .disableHtmlEscaping()
                 .create()
                 .toJson(json);
-        if ( file == null || json == null) {
+        if ( file == null || json == null)
             return;
-        }
+
         if (Objects.equals(type, "write")) {
             try {
                 outStream = new FileOutputStream(file);
@@ -90,32 +90,39 @@ public class EffWriteToFile extends Effect {
                 reader.close();
                 if ( element.isJsonNull())
                     return;
+
                 if ( element.isJsonObject() ) {
                     if ( !k ) {
                         int size = element.getAsJsonObject().entrySet().size();
                         element.getAsJsonObject().add(String.valueOf(size), json);
-                        outStream = new FileOutputStream(file);
-                        jsonWriter = new JsonWriter(new OutputStreamWriter(outStream, StandardCharsets.UTF_8));
-                        jsonWriter.jsonValue(new GsonBuilder()
-                                .disableHtmlEscaping().setPrettyPrinting().create().toJson(element)
-                        );
-                        jsonWriter.flush();
-                        jsonWriter.close();
                     } else {
-                        /**
-                         * TODO input jsonelement by users key input
-                         * ? Enchantment -> search the user key input in array -> Try Append
-                         */
-                        System.out.println(inKey);
+                        Map<String, JsonElement> jeMap = new HashMap<>();
+                        if (json.isJsonObject()) {
+                            for (Map.Entry<String, JsonElement> entry : json.getAsJsonObject().entrySet()) {
+                                System.out.println("Key: " + entry.getKey());
+                                System.out.println("Value: " + entry.getValue());
+                                jeMap.put(entry.getKey(), entry.getValue());
+                            }
+                            assert key != null;
+                            element.getAsJsonObject().add(key, JsonParser.parseString(new Gson().toJson(jeMap)));
+                        } else if ( json.isJsonArray()) {
+                            for(int i = 0; json.getAsJsonArray().size() > i; i++) {
+                                element.getAsJsonObject().add(String.valueOf(i), json.getAsJsonArray().get(i));
+                            }
+                        }
                     }
+                } else if (element.isJsonArray() ) {
+                    element.getAsJsonArray().add(json);
                 }
-            }catch (Exception ex) {ex.printStackTrace();}
+                outStream = new FileOutputStream(file);
+                jsonWriter = new JsonWriter(new OutputStreamWriter(outStream, StandardCharsets.UTF_8));
+                jsonWriter.jsonValue(new GsonBuilder()
+                        .disableHtmlEscaping().setPrettyPrinting().create().toJson(element)
+                );
+                jsonWriter.flush();
+                jsonWriter.close();
+            }catch (IOException ex) {ex.printStackTrace();}
         }
-
-
-
-
-
     }
 
     @Override
@@ -126,17 +133,16 @@ public class EffWriteToFile extends Effect {
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
         jsonelement = (Expression<JsonElement>) exprs[0];
-        SkriptGson.info("IN");
-        if (parseResult.tags.contains("write")) {
+        if (parseResult.hasTag("write")) {
             type = "write";
-        } else if (parseResult.tags.contains("put")) {
+        } else if (parseResult.hasTag("put")) {
             type = "put";
         }
-        object = (Expression<File>) exprs[1];
-        if (parseResult.tags.contains("as key")) {
+        object = LiteralUtils.defendExpression(exprs[1]);
+        if (parseResult.hasTag("as")) {
             k = true;
             inKey = (Expression<String>) exprs[2];
         }
-        return true;
+        return LiteralUtils.canInitSafely(object);
     }
 }
