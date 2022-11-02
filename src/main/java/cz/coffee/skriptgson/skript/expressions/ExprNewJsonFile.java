52a51a6c6960f4330b0fb8687abf323e327bf762
cooffeeRequired <106232282+cooffeeRequired@users.noreply.github.com>
2022-11-02 15:38:57 +0100

    ** Fixed initial version, returning New File object.

--- src/main/java/cz/coffee/skriptgson/skript/expressions/ExprNewJsonFile.java
+++ src/main/java/cz/coffee/skriptgson/skript/expressions/ExprNewJsonFile.java
@@ -2,6 +2,7 @@ package cz.coffee.skriptgson.skript.expressions;
 
 import ch.njol.skript.Skript;
 import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Expression;
@@ -12,7 +13,6 @@ import ch.njol.util.Kleenean;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
-import com.google.gson.JsonSyntaxException;
 import com.google.gson.stream.JsonWriter;
 import cz.coffee.skriptgson.SkriptGson;
 import org.bukkit.event.Event;
@@ -20,13 +20,17 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 
 @Name("New json File")
 @Description("Creates a new JSON file")
 @Since("1.0")
+@Examples({
+        "set {-e} to new json file \"YouAreAwesome\\test.json\"",
+        "set {-e} to new json file \"YouAreAwesome\\test.json\" with data (json {\"A\":\"B\"})",
+        "set {-e} to new json file \"YouAreAwesome\\test.json\" with data (json from string \"{'Im': 'God'}\")",
+})
 
 
 @SuppressWarnings({"unchecked","unused","NullableProblems"})
@@ -34,25 +38,26 @@ public class ExprNewJsonFile extends SimpleExpression<Object> {
 
     static {
         Skript.registerExpression(ExprNewJsonFile.class,Object.class, ExpressionType.COMBINED,
-                "[a] new json-file %string% [(:with [data] %-string%)]");
+                "[a] new json file %string% [(:with [data] %-string/jsonelement%)]");
 
     }
 
     private Expression<String> exprString;
-    private Expression<String> exprData;
-    private int pattern;
+    private Expression<String> exprRawData;
+    private boolean withData;
 
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
         exprString = (Expression<String>) exprs[0];
-        pattern = matchedPattern;
         if (!parseResult.tags.isEmpty()) {
-            exprData = (Expression<String>) exprs[1];
+            exprRawData = (Expression<String>) exprs[1];
+            withData = true;
         }
         return true;
     }
 
     public File[] get(@NotNull Event event) {
+
         if (exprString == null)
             return null;
         String inputFile = exprString.getSingle(event);
@@ -62,13 +67,15 @@ public class ExprNewJsonFile extends SimpleExpression<Object> {
         FileOutputStream OutputStream;
         JsonWriter writer;
         JsonElement parsedString;
-        String inputData;
+        Object inputData;
 
-        inputData = (exprData != null ? exprData.getSingle(event) : null);
+        inputData = (exprRawData != null ? exprRawData.getSingle(event) : null);
 
         if (new File(inputFile).exists()){
-            SkriptGson.warning("&r&ccan't create the file &e" + inputFile + ",&c because is already exist");
-        return null;
+            if(new File(inputFile).length() >1) {
+                SkriptGson.warning("&r&ccan't create the file &e" + inputFile + ",&c because is already exist");
+                return null;
+            }
         }
 
         try {
@@ -78,20 +85,18 @@ public class ExprNewJsonFile extends SimpleExpression<Object> {
             writer.jsonValue(new GsonBuilder()
                     .setPrettyPrinting()
                     .create()
-                    .toJson(
-                            JsonParser.parseString(inputData == null ? "{}" : inputData)
-                    )
+                    .toJson(inputData instanceof JsonElement ? inputData : JsonParser.parseString(inputData == null ? "{}" : (String) inputData))
             );
             writer.flush();
             writer.close();
-        } catch (IOException | JsonSyntaxException e) {
+        } catch (Exception e) {
             return null;
         }
         return new File[]{new File(inputFile)};
     }
     @Override
     public String toString(Event event,boolean debug) {
-        return "json file " + (pattern == 0 ? "" : "with data");}
+        return "json file " + (!withData ? "" : "with data");}
 
     @Override
     public boolean isSingle() {
