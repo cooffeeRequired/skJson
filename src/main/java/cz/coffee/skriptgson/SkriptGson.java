9baae699fb69d9dd6e135cedc8a8794196cb2c22
cooffeeRequired <106232282+cooffeeRequired@users.noreply.github.com>
2022-11-06 20:00:44 +0100

    conversion fix issue #2

--- src/main/java/cz/coffee/skriptgson/SkriptGson.java
+++ src/main/java/cz/coffee/skriptgson/SkriptGson.java
@@ -41,7 +41,7 @@ public class SkriptGson extends JavaPlugin {
             return;
         }
         info("&aFinished loading.");
-        info("Build: #23e22");
+        info("Build: &r&f#23e40");
 
     }
 
