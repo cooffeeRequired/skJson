2d4cc9d186b071321e24f3eb043f106530e0ffd3
cooffeeRequired <106232282+cooffeeRequired@users.noreply.github.com>
2022-11-08 23:59:31 +0100

    Json has Key/Value's -> Fixed issue @

--- src/main/java/cz/coffee/skriptgson/SkriptGson.java
+++ src/main/java/cz/coffee/skriptgson/SkriptGson.java
@@ -41,7 +41,7 @@ public class SkriptGson extends JavaPlugin {
             return;
         }
         info("&aFinished loading.");
-        info("Build: &r&f#23f20");
+        info("Build: &r&f#33f20");
 
     }
 
