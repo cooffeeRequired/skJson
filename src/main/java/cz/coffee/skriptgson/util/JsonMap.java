94abcd5b33957fb69f26229b7bb2d6d2c6d2867c
cooffeeRequired <106232282+cooffeeRequired@users.noreply.github.com>
2022-11-10 12:20:37 +0100

    JsonMap Updated -> @fe200

--- src/main/java/cz/coffee/skriptgson/util/JsonMap.java
+++ src/main/java/cz/coffee/skriptgson/util/JsonMap.java
@@ -17,16 +17,17 @@ import static cz.coffee.skriptgson.util.PluginUtils.newGson;
 
 public class JsonMap {
 
+    private int i;
+
     public static boolean checkObject(JsonObject jElem, int type, Object ...input) {
         for (Map.Entry<String, JsonElement> entry : jElem.entrySet()) {
             JsonElement element = entry.getValue();
             JsonElement j;
-
             if ( type == 1) {
                 if (Objects.equals(entry.getKey(), input[0]))
                     return true;
             } else if ( type == 2){
-                if(input[0].toString().contains("{")) {
+                if(input[0].toString().startsWith("{") || input[0].toString().startsWith("[")) {
                     j = JsonParser.parseString(input[0].toString());
                 } else {
                     j = newGson().toJsonTree(input[0]);
@@ -65,11 +66,66 @@ public class JsonMap {
         return false;
     }
 
+    public JsonMap countOccurrenceOfKey(final JsonElement jel, String key, int ...type) {
+        final JsonObject object;
+        final JsonArray array;
+        JsonElement n;
+        if(jel.isJsonArray()) {
+            array = jel.getAsJsonArray();
+            for (int index = 0; index < array.size(); index++){
+                JsonElement j = array.get(index);
+                if ( type[0] == 1) {
+                    if(Objects.equals(key, j.toString())) {
+                        this.i++;
+                    }
+                } else if ( type[0] == 2) {
+                    if(key.startsWith("{") || key.startsWith("[")) {
+                        n = JsonParser.parseString(key);
+                    } else {
+                        n = newGson().toJsonTree(key);
+                    }
+                    if(Objects.equals(n, j)) {
+                        this.i++;
+                    }
+                }
+                if (j.isJsonObject()) {
+                    countOccurrenceOfKey(j.getAsJsonObject(), key, type[0]);
+                } else if (j.isJsonArray()) {
+                    countOccurrenceOfKey(j.getAsJsonArray(), key, type[0]);
+                }
+            }
+        } else if (jel.isJsonObject()) {
+            object = jel.getAsJsonObject();
+            for (Map.Entry<String, JsonElement> entry : object.entrySet()) {
+                JsonElement j = entry.getValue();
 
+                if ( type[0] == 1) {
+                    if(Objects.equals(key, entry.getKey())) {
+                        this.i++;
+                    }
+                } else if ( type[0] == 2) {
+                    if(key.startsWith("{") || key.startsWith("[")) {
+                        n = JsonParser.parseString(key);
+                    } else {
+                        n = newGson().toJsonTree(key);
+                    }
+                    if(Objects.equals(n, j)) {
+                        this.i++;
+                    }
+                }
+                if(j.isJsonObject()) {
+                    countOccurrenceOfKey(j.getAsJsonObject(), key, type[0]);
+                } else if(j.isJsonArray()) {
+                    countOccurrenceOfKey(j.getAsJsonArray(), key, type[0]);
+                }
+            }
+        }
+        return this;
+    }
 
-
-
-
+    public int getCount() {
+        return this.i;
+    }
 
     public static void parseJsonArray(JsonArray jArray) {
         for (int index = 0; index < jArray.size(); index++) {
