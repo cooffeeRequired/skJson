3eadb714f789fd9d167bcc31e5f9d59aa0bfa788
cooffeeRequired <106232282+cooffeeRequired@users.noreply.github.com>
2022-11-26 22:17:33 +0100

    Add GitHub version checker

--- src/main/java/cz/coffee/skriptgson/util/Utils.java
+++ src/main/java/cz/coffee/skriptgson/util/Utils.java
@@ -6,8 +6,15 @@ package cz.coffee.skriptgson.util;
 import ch.njol.yggdrasil.YggdrasilSerializable;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
+import cz.coffee.skriptgson.SkriptGson;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 
 @SuppressWarnings("unused")
 public class Utils {
@@ -15,10 +22,10 @@ public class Utils {
     public static String color(String string) {
         return ChatColor.translateAlternateColorCodes('&', string);
     }
-    public static String sanitizeString(Object stringObject) {
+    public static String SanitizeString(Object stringObject) {
         return String.valueOf(stringObject).replaceAll("[\"'][\\w\\s]+[\"']|\\w+[\"']\\w+", "").replaceAll("\"", "");
     }
-    public static String sanitizeJson(Object stringObject) {
+    public static String SanitizeJson(Object stringObject) {
         return String.valueOf(stringObject).replaceAll("^[{}$]", "");
     }
     public static String gsonText(Object StringifyElement) {
@@ -33,6 +40,38 @@ public class Utils {
                 .registerTypeHierarchyAdapter(ConfigurationSerializable.class, new BukkitClassAdapt())
                 .registerTypeHierarchyAdapter(YggdrasilSerializable.class, new SkriptClassAdapt());
        return g.disableHtmlEscaping().setPrettyPrinting().create();
+    }
+
+    public static String getGitVersion() {
+        try {
+            URL url = new URL("https://api.github.com/repos/cooffeeRequired/skript-gson/releases/latest");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty("Accept", "application/vnd.github+json");
+            conn.setDoOutput(true);
+            conn.setRequestMethod("GET");
+            conn.connect();
+            int response = conn.getResponseCode();
+            if (response == 200) {
+                StringBuilder inLine = new StringBuilder();
+                Scanner scanner = null;
+                try {
+                    scanner = new Scanner(url.openStream());
+                    while(scanner.hasNext())
+                        inLine.append(scanner.nextLine());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } finally {
+                    assert scanner != null;
+                    scanner.close();
+                }
+                return JsonParser.parseString(inLine.toString()).getAsJsonObject().get("tag_name").toString().replaceAll("\"", "");
+            }
+            SkriptGson.warning("GitHubRelease response code " + response);
+            SkriptGson.warning("We can't check the version, check your connection");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 
 }
