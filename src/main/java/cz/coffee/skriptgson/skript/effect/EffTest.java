11e04f9e0f7c7ebcde59156ba7656700b6022f0e
cooffeeRequired <106232282+cooffeeRequired@users.noreply.github.com>
2022-11-27 20:00:45 +0100

    new Features, Important changes.

--- src/main/java/cz/coffee/skriptgson/skript/effect/EffTest.java
+++ src/main/java/cz/coffee/skriptgson/skript/effect/EffTest.java
@@ -16,31 +16,44 @@ import java.util.Objects;
 
 public class EffTest extends Effect {
 
-    private Expression<String > code;
+    private Expression<String> code;
+    private Expression<Objects> data;
 
     static {
-        Skript.registerEffect(EffTest.class, "test skript-gson %string%");
+        Skript.registerEffect(EffTest.class, "test skript-gson %string% %-objects%");
     }
 
     @Override
     protected void execute(@NotNull Event e) {
 
         String code2 = code.getSingle(e);
+        Object[] data2 = data.getAll(e);
+        JsonElement json = JsonParser.parseString("{'A': {'B': {}}}");
+        JsonElement parse = JsonParser.parseString("{'A': 'XXXXXXXXXXX'}");
 
-        if(Objects.equals(code2, "new utils")) {
-            JsonElement json = JsonParser.parseString("{'A': {'B': {}}}");
-            JsonElement parse = JsonParser.parseString("{'A': 'XXXXXXXXXXX'}");
+        if (Objects.equals(code2, "new utils")) {
+            System.out.println("APPEND " + GsonUtils.append(json, parse, null, "A:B"));
 
-            System.out.println("APPEND "+ GsonUtils.append(json, parse, null, "A:B"));
-
-            System.out.println("CHECK KEY "+ GsonUtils.check(json, "B", GsonUtils.Type.KEY));
-            System.out.println("CHECK VALUE NON JSON "+ GsonUtils.check(json, "1", GsonUtils.Type.VALUE));
-            System.out.println("CHECK VALUE JSON "+ GsonUtils.check(json, "{'B': 1}", GsonUtils.Type.VALUE));
+            System.out.println("CHECK KEY " + GsonUtils.check(json, "B", GsonUtils.Type.KEY));
+            System.out.println("CHECK VALUE NON JSON " + GsonUtils.check(json, "1", GsonUtils.Type.VALUE));
+            System.out.println("CHECK VALUE JSON " + GsonUtils.check(json, "{'B': 1}", GsonUtils.Type.VALUE));
 
 
-            System.out.println("Change KEY "+ GsonUtils.change(json, "A", "Y", GsonUtils.Type.KEY));
-            System.out.println("Change VALUE "+ GsonUtils.change(json, "B", parse, GsonUtils.Type.VALUE));
-        } else {
+            System.out.println("Change KEY " + GsonUtils.change(json, "A", "Y", GsonUtils.Type.KEY));
+            System.out.println("Change VALUE " + GsonUtils.change(json, "B", parse, GsonUtils.Type.VALUE));
+        } else if (Objects.equals(code2, "listToJson")) {
+            if (data2[0] != null)
+                System.out.println(GsonUtils.GsonMapping.listToJson(e, data2[0].toString().substring(0, data2[0].toString().length() - 1)));
+            else
+                Skript.debug("Do you forgot the name of variable?");
+        } else if (Objects.equals(code2, "jsonToList")) {
+            if (data2[0] != null) {
+                String K = data2[0].toString().substring(0, data2[0].toString().length() - 3);
+                GsonUtils.GsonMapping.jsonToList(e, K, json, true);
+            } else {
+                Skript.debug("Do you forgot the name of variable?");
+            }
+
         }
     }
 
@@ -52,6 +65,7 @@ public class EffTest extends Effect {
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
         code = (Expression<String>) exprs[0];
+        data = (Expression<Objects>) exprs[1];
         return true;
     }
 }
