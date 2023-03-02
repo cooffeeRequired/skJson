c8327b68f7886d4afd7e7f5643d69f172952a3b4
coffeeRequired <nhl-jurasek@seznam.cz>
2023-03-02 09:04:24 +0100

    Create CondCachedJsonExist.java

---
+++ src/main/java/cz/coffee/skript/conditions/CondCachedJsonExist.java
@@ -0,0 +1,59 @@
+package cz.coffee.skript.conditions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Condition;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
+import org.jetbrains.annotations.NotNull;
+
+import static cz.coffee.SkJson.FILE_JSON_MAP;
+import static cz.coffee.SkJson.JSON_STORAGE;
+
+@Name("Cached json exist")
+@Description("You can check if the cached json exist")
+@Examples({"on load:",
+        "\tif cached json doesn't exists:",
+        "\t\tdefine new cached json \"test1\" for given path \"test.json\""
+})
+@Since("2.7.0 - define new cached json")
+
+
+public class CondCachedJsonExist extends Condition {
+
+    static {
+        Skript.registerCondition(CondCachedJsonExist.class,
+                "cached json %string% exists",
+                        "cached json %string% does(n't| not) exists"
+        );
+    }
+
+    private Expression<String> exprName;
+    private boolean negated;
+
+    @Override
+    public boolean check(@NotNull Event e) {
+        String name = exprName.getSingle(e);
+        return (!negated) == JSON_STORAGE.containsKey(name);
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "cached json "+ exprName.toString(e, debug)+(negated ? " does not exists" : " exists");
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, @NotNull ParseResult parseResult) {
+        negated = matchedPattern == 1;
+        exprName = (Expression<String>) exprs[0];
+        setNegated(negated);
+        return true;
+    }
+}
