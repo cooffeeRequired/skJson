beee2b1499defa1ca78672c837e10938ad0e23e9
coffeeRequired <nhl-jurasek@seznam.cz>
2023-04-13 09:17:12 +0200

    Version 2.8.2 - Small fixing colors in Pretty Print

--- src/main/java/cz/coffee/core/ColoredJson.java
+++ src/main/java/cz/coffee/core/ColoredJson.java
@@ -4,6 +4,9 @@ package cz.coffee.core;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
+import cz.coffee.core.utils.Util;
+
+import static cz.coffee.core.ColoredJson.Colors.*;
 
 /**
  * This file is part of skJson.
@@ -25,8 +28,32 @@ import com.google.gson.JsonElement;
  * <p>
  * Created: Saturday (3/10/2023)
  */
-
+@SuppressWarnings("unused")
 public class ColoredJson {
+
+    enum Colors {
+        DARK_BLUE(Util.color("&1")),
+        DARK_GREEN(Util.color("&2")),
+        DARK_AQUA(Util.color("&3")),
+        DARK_RED(Util.color("&4")),
+        DARK_PURPLE(Util.color("&5")),
+        GOLD(Util.color("&6")),
+        GRAY(Util.color("&7")),
+        DARK_GRAY(Util.color("&8")),
+        BLUE(Util.color("&9")),
+        GREEN(Util.color("&a")),
+        AQUA(Util.color("&b")),
+        RED(Util.color("&c")),
+        PURPLE(Util.color("&d")),
+        YELLOW(Util.color("&e")),
+        WHITE(Util.color("&f"));
+        final String color;
+        Colors(String st) {
+            color = st;
+        }
+
+    }
+
     final static Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().serializeNulls().create();
     private String finalOutput;
 
@@ -35,18 +62,21 @@ public class ColoredJson {
     }
 
     private void process(JsonElement input) {
+
+        final String RESET = Util.color("&r");
+
         if (input == null) {
             return;
         }
         StringBuilder jsonString = new StringBuilder(gson.toJson(input));
         finalOutput = new String(jsonString)
-                        .replaceAll("(?<=\\W)([+]?([0-9]*[.])?[0-9]+)", "§b$1§f")
-                        .replaceAll("(?i:true)", "§a$0§f")
-                        .replaceAll("(?i:false)", "§c$0§f")
-                        .replaceAll("(\")(.)", "§8$1§f$2§f")
-                        .replaceAll("([{}])|([\\[\\]])", "§7$1§e$2§f"
-                        );
+                        .replaceAll("(?<=\\W)([+]?([0-9]*[.])?[0-9]+)", AQUA.color + "$1" + WHITE.color)
+                        .replaceAll("(?i:true)", GREEN.color + "$0" + WHITE.color)
+                        .replaceAll("(?i:false)", RED.color+"$0"+WHITE.color)
+                        .replaceAll("(\")((.)|)", DARK_GRAY.color + "$1" + WHITE.color + "$2" + WHITE.color)
+                        .replaceAll("([{}])|([\\[\\]])",GRAY.color + "$1" + YELLOW.color + "$2" + WHITE.color);
     }
+
 
 
     public String getOutput() {
