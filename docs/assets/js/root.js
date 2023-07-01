47ee067085c744ebc110ea7b82b10983b4eae25f
coffeeRequired <106232282+cooffeeRequired@users.noreply.github.com>
2023-07-01 13:34:29 +0200

    docs

--- docs/assets/js/root.js
+++ docs/assets/js/root.js
@@ -1,17 +1,79 @@
-// main page.html
-function saveDataAsJSON(data) {
-    var jsonData = JSON.stringify(data);
-    var downloadLink = document.createElement('a');
-    downloadLink.href = 'data:text/json;charset=utf-8,' + encodeURIComponent(jsonData);
-    downloadLink.download = 'data.json';
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-    document.body.removeChild(downloadLink);
-  }
+const pages = {
+    "get-started": {
+        title: 'Get Started',
+        color: '#0000',
+        html: `
+        <div class="container-page-docs">
+            <p> something nice </p>
+            #code
+        </div>
+        `,
+        codes: [
+            {
+                't': 'javascript',
+                'code': 
+                `
+                    function greeting(name) {
+                        console.log('Hello, ' + name + '!');
+                    }
+                
+                    greeting('John');
+                `
+            }
+        ]
+    },
+    "about-coder": {
+        title: 'About coder',
+        color: '#0000',
+        html: `
+        <div class="container-page">
+            <p>My name is Jiri, I'm 24 years old. and for a long time I programmed in PHP. about a year ago I started to be interested in other languages such as Ruby, Java, C# and in addition at that time I was the owner of one of the bigger CZ/SK minecraft server so I was constantly digging in plugins until one day I came across the plugin Skript and I started to use it after a while I got the idea to create my own plugin and it was just SkJson. now they are working on other projections...</p>
+            <h4>Where am I from?</h4>
+            map here
+            Discord
+        </div>`,
+    },
+    "about-skjson": {
+        title: 'About SkJson',
+        color: '#0000',
+        html: `
+        <div class="container-page">
+            <p>SkJson is a library for working with JSON in Skript,
+            it supports all known versions of minecraft as well as server types (Paper, Purpur, Spigot).<p>
+            <p>With SkJson you can easily read and write JSON data, traverse JSON structures and perform various operations on JSON objects and arrays.</p>
+            <p>SkJson is designed to be lightweight and efficient, making it a great choice for working with JSON data in scripts. It was created last year.</p>
+            <p>It is primarily an add-on to the well-known Skript plugin.</p>
+        </div>`,
+    }
+}
 
+// main page.html
 // Sidebar toggle functionality
-const articleData = [
-]
+
+function changeSelectedPage(pageCH=String) {
+    const page = document.getElementById('page-content');
+    const pageTitle = document.getElementById('page-name');
+    if(pages[pageCH] != null) {
+        pageTitle.innerHTML = pages[pageCH].title
+        if (pageCH === 'get-started') {
+            let cs = pages[pageCH].codes
+            if (cs.length != 0) {
+                // const pagedocs = document.querySelectorAll('.container-page-docs')
+                // const pre = document.createElement('pre')
+                // const code = document.createElement('code')
+
+                // pagedocs.appendChild(pre)
+                // pagedocs.appendChild(code)
+
+            }
+
+        }
+        page.innerHTML = pages[pageCH].html
+    }
+}
+
+const articleData = []
+
 document.addEventListener('DOMContentLoaded', () => {
 
     const sidebarToggle = document.getElementById('sidebar-toggle');
@@ -55,25 +117,32 @@ function addArticle(title, image, url, text) {
     })
 }
 
-// Funkce pro rozšiřování článků
-function expandArticle(event) {
-    const articleContent = event.target.parentNode;
-    articleContent.classList.toggle('article-expanded');
-}
-
 // Zaregistrujeme událost kliknutí na všechny odkazy "Read More"
 document.addEventListener('DOMContentLoaded', () => {
     const readMoreLinks = document.querySelectorAll('.article-read-more');
     readMoreLinks.forEach((link) => {
         link.addEventListener('click', expandArticle);
     });
+
+    getData().then(() => {
+        loadArticles()
+    })
 });
 
 function loadArticles() {
     const articlesContainer = document.querySelector('.articles-container');
+    const art = document.getElementById('article-latest');
     if (articleData.length === 0) {
-
+        art.classList.remove("article-latest")
+        art.classList.add("article-latest-notfound")
+        art.textContent = "Nothing here"
     } else {
+        // change article-latest
+        art.classList.add("article-latest")
+        art.classList.remove("article-latest-notfound")
+        art.textContent = "Latest"
+        
+
         // Create article elements and append them to the container
         articleData.forEach((article) => {
             const articleElement = document.createElement('div');
@@ -96,7 +165,7 @@ function loadArticles() {
 
             const articleReadMore = document.createElement('a');
             articleReadMore.classList.add('article-read-more');
-            articleReadMore.textContent = 'Read More';
+            articleReadMore.textContent = 'Read more on github';
             articleReadMore.href = article.url;
 
             articleContent.appendChild(articleTitle);
@@ -111,11 +180,38 @@ function loadArticles() {
     }
 }
 
+function cleanMD(mdString, maxSize) {
+    const converter = new showdown.Converter();
+    let cleanText = converter.makeHtml(mdString);;
+    let cleanen = cleanText.replace(/<\/?[^>]+(>|$)/g, '');
+    if (cleanen.length >= 100) cleanen = cleanen.slice(0, maxSize);
+    cleanen = cleanen.replace(/<code>/g, '&lt;code&gt;');
+    cleanen = cleanen.replace(/<\/code>/g, '&lt;/code&gt;');
+    return cleanen;
+}
 
-// Call the loadArticles function to dynamically load articles
-document.addEventListener('DOMContentLoaded', () => {
-    addArticle("2.8.6", "../assets/fav.png", "https://github.com/SkJsonTeam/skJson/releases/tag/2.8.6", "Fixed nulls elements by (SET/ADD).Added effect for load json files from folder");
-    addArticle("2.8.5", "../assets/fav.png", "https://github.com/SkJsonTeam/skJson/releases/tag/2.8.5", "Fixed java issue and file writing");
-    loadArticles();
-    saveDataAsJSON(articleData)
-});
+
+
+async function getData() {
+    let static_logo =  "assets/fav.png"
+    let max_lengh  = 6;
+    let array = []
+    fetch('https://api.github.com/repos/SkJsonTeam/SkJson/releases', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/vnd.github+json'
+        }
+    }).then(rsp => rsp.json()).then(json => {
+        for (var i = 0; i < max_lengh; i++) {
+            let j = json[i]
+            addArticle
+            (
+                j['tag_name'],
+                static_logo,
+                j['html_url'],
+                cleanMD(j['body'], 200)
+            )
+        }
+        loadArticles()
+    })
+}
