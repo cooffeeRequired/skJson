332215c9f17aad1e3dcc2cf955019e25f9461f39
SkJson <130649230+SkJsonTeam@users.noreply.github.com>
2023-07-01 14:07:47 +0200

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/docs',
+  base: '/skJson/',
   title: "SkJson docs",
   description: "A VitePress Site",
   themeConfig: {
