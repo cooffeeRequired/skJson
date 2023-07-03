8e37e473d8eeb4ce0c41d0e27267d2c568a1e510
coffeeRequired <106232282+cooffeeRequired@users.noreply.github.com>
2023-07-03 13:46:23 +0200

    update docs

--- docs_/.vitepress/config.js
+++ docs_/.vitepress/config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Examples', link: '/markdown-examples' },
-      { text: 'Home page', link: '/skJson/web'}
+      { text: 'Home page', link: '/skJson/'}
     ],
 
     sidebar: [
