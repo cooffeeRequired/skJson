82408d450e4d2313c8ed6ca76db33d5ab982dbfd
coffeeRequired <106232282+cooffeeRequired@users.noreply.github.com>
2023-07-03 13:39:50 +0200

    docs up

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,14 +2,15 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/skJson/',
+  base: '/skJson/docs',
   title: "SkJson docs",
   description: "A VitePress Site",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/markdown-examples' },
+      { text: 'Home page', link: '/skJson/web'}
     ],
 
     sidebar: [
