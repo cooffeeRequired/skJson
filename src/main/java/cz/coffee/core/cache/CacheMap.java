8fe1ce0365f5cefb98f5da65288320fca0614f0d
SkJson <130649230+SkJsonTeam@users.noreply.github.com>
2023-09-14 23:51:18 +0200

    Delete src/main directory

--- src/main/java/cz/coffee/core/cache/CacheMap.java
+++
@@ -1,9 +0,0 @@
-package cz.coffee.core.cache;
-
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
-@SuppressWarnings("ALL")
-public class CacheMap<T, K, V> extends ConcurrentHashMap<T, Map<K, V>> {
-
-}
