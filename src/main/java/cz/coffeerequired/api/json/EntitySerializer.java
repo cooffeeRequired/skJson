7a78f89926ff978369cf0d8af81a12ce317eb224
coffee <nhl-jurasek@seznam.cz>
2025-02-03 10:36:32 +0100

    feat: support-entities, add new EntitySerializer for serialize/deserialize entities feat: change x,y,z,pitch, etc. to Location object as well

--- src/main/java/cz/coffeerequired/api/json/EntitySerializer.java
+++ src/main/java/cz/coffeerequired/api/json/EntitySerializer.java
@@ -1,65 +1,41 @@
 package cz.coffeerequired.api.json;
 
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
-import org.bukkit.Bukkit;
+import com.google.gson.*;
+import cz.coffeerequired.api.Api;
+import cz.coffeerequired.api.nbts.NBTConverter;
+import de.tr7zw.changeme.nbtapi.NBT;
+import de.tr7zw.changeme.nbtapi.NBTContainer;
+import de.tr7zw.changeme.nbtapi.NBTEntity;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
-import org.bukkit.NamespacedKey;
-import org.bukkit.persistence.PersistentDataType;
-import org.bukkit.plugin.Plugin;
+import org.bukkit.entity.Player;
 
 import java.lang.reflect.Type;
-import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
+@SuppressWarnings("deprecation")
 public class EntitySerializer implements JsonSerializer<Entity>, JsonDeserializer<Entity> {
 
     @Override
     public JsonElement serialize(Entity src, Type typeOfSrc, JsonSerializationContext context) {
         JsonObject obj = new JsonObject();
-        // Uložíme UUID entity
         obj.addProperty("uuid", src.getUniqueId().toString());
-        // Uložíme název světa, kde se entita nachází
-        obj.addProperty("world", src.getWorld().getName());
-        // Uložíme typ entity
         obj.addProperty("type", src.getType().toString());
+        obj.add("location", GsonParser.toJson(src.getLocation()));
 
-        // Uložíme pozici entity
-        Location loc = src.getLocation();
-        obj.addProperty("x", loc.getX());
-        obj.addProperty("y", loc.getY());
-        obj.addProperty("z", loc.getZ());
-        // Uložíme natočení entity (yaw a pitch)
-        obj.addProperty("yaw", loc.getYaw());
-        obj.addProperty("pitch", loc.getPitch());
-
-        // Uložíme jméno entity – pokud má custom name, uložíme jej;
-        // pokud jde o hráče, můžeme uložit jeho jméno (pozn.: hráčovo jméno se zpravidla nemění)
-        if (src.getCustomName() != null) {
-            obj.addProperty("customName", src.getCustomName());
-        } else if (src instanceof org.bukkit.entity.Player) {
-            obj.addProperty("name", ((org.bukkit.entity.Player) src).getName());
+        if (src.customName() != null) {
+            obj.addProperty("customName", PlainTextComponentSerializer.plainText().serialize(Objects.requireNonNull(src.customName())));
+        } else if (src instanceof Player player) {
+            obj.addProperty("name", player.getName());
         }
 
-        // Serializace persistent data – předpokládáme, že hodnoty jsou typu String
-        if (src.getPersistentDataContainer() != null && !src.getPersistentDataContainer().getKeys().isEmpty()) {
-            JsonObject persistentDataObj = new JsonObject();
-            for (NamespacedKey key : src.getPersistentDataContainer().getKeys()) {
-                // Zde předpokládáme, že data jsou uložena jako String; v případě jiných typů je třeba přidat podporu
-                String value = src.getPersistentDataContainer().get(key, PersistentDataType.STRING);
-                if (value != null) {
-                    // Uložíme klíč ve formátu "namespace:key"
-                    persistentDataObj.addProperty(key.getNamespace() + ":" + key.getKey(), value);
-                }
-            }
-            obj.add("persistentData", persistentDataObj);
+        if (Api.Records.PROJECT_ENABLED_NBT) {
+            NBTEntity nbtEntity = new NBTEntity(src);
+            JsonElement json = NBTConverter.toJson(new NBTContainer(nbtEntity.getCompound()));
+            obj.add("nbt", json);
         }
 
         return obj;
@@ -69,40 +45,27 @@ public class EntitySerializer implements JsonSerializer<Entity>, JsonDeserialize
     public Entity deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         JsonObject obj = json.getAsJsonObject();
 
-        // Načteme UUID entity
         String uuidStr = obj.get("uuid").getAsString();
         UUID uuid = UUID.fromString(uuidStr);
+        Location loc = GsonParser.fromJson(obj.getAsJsonObject("location"));
 
-        // Načteme název světa
-        String worldName = obj.get("world").getAsString();
-        World world = Bukkit.getWorld(worldName);
-        if (world == null) {
-            throw new JsonParseException("Svět " + worldName + " nebyl nalezen!");
-        }
-
-        // Pokusíme se získat entitu podle UUID
+        assert loc != null;
+        World world = loc.getWorld();
         Entity entity = world.getEntity(uuid);
+
         if (entity == null) {
-            throw new JsonParseException("Entita s UUID " + uuid + " nebyla ve světě " + worldName + " nalezena!");
+            throw new JsonParseException("Entity with UUID " + uuid + " could not be found");
         }
         if (obj.has("customName")) {
             entity.setCustomName(obj.get("customName").getAsString());
         }
-        if (obj.has("persistentData")) {
-            JsonObject persistentDataObj = obj.getAsJsonObject("persistentData");
-            for (Map.Entry<String, JsonElement> entry : persistentDataObj.entrySet()) {
-                String keyStr = entry.getKey(); // formát "namespace:key"
-                String value = entry.getValue().getAsString();
-                String[] parts = keyStr.split(":");
-                if (parts.length == 2) {
-                    String namespace = parts[0];
-                    String keyName = parts[1];
-                    Plugin plugin = Bukkit.getPluginManager().getPlugin(namespace);
-                    if (plugin != null) {
-                        NamespacedKey key = new NamespacedKey(plugin, keyName);
-                        entity.getPersistentDataContainer().set(key, PersistentDataType.STRING, value);
-                    }
-                }
+        if (obj.has("nbt")) {
+            if (Api.Records.PROJECT_ENABLED_NBT) {
+                NBTContainer cont = NBTConverter.fromJson(obj.getAsJsonObject("nbt"));
+                NBT.modify(entity, nbt -> {
+                    nbt.clearNBT();
+                    nbt.mergeCompound(cont);
+                });
             }
         }
 
