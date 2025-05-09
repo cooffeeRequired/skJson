582dd54679dc8347783dd8457efc42cd304bed3d
cooffeeRequired <nhl-jurasek@seznam.cz>
2025-05-09 15:22:12 +0200

    add: fallback feature

---
+++ src/main/java/cz/coffeerequired/fallback/FallBack.java
@@ -0,0 +1,103 @@
+package cz.coffeerequired.fallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.stream.Stream;
+
+import cz.coffeerequired.SkJson;
+import cz.coffeerequired.api.Api;
+import cz.coffeerequired.api.Extensible;
+
+public class FallBack {
+    private final FallbackProperties fallbackProperties;
+    private final File dataFolder = SkJson.getInstance().getDataFolder();
+    
+
+    public FallBack() {
+        this.fallbackProperties = FallbackProperties.getInstance();
+        this.fallback();
+    }
+
+    private void containsFallback(File jarFile) throws Exception {
+        URL jarURL = jarFile.toURI().toURL();
+        // 1. Načti JAR pomocí URLClassLoader
+        try (URLClassLoader classLoader = new URLClassLoader(new URL[]{jarURL}, SkJson.class.getClassLoader());
+             JarFile jar = new JarFile(jarFile)) {
+
+            Enumeration<JarEntry> entries = jar.entries();
+            while (entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                String name = entry.getName();
+
+                if (name.endsWith(".class") && !entry.isDirectory()) {
+                    String className = name.replace('/', '.').replace(".class", "");
+                    try {
+                        Class<?> cls = classLoader.loadClass(className);
+                        if (Extensible.class.isAssignableFrom(cls)) {
+                            SkJson.getRegister().registerModule(cls.asSubclass(Extensible.class));
+                        }
+                    } catch (Throwable t) {
+                        SkJson.exception(t, "Failed to load class: %s", className);
+                    }
+                }
+            }
+        }
+        
+    }
+
+    private String getFallbackVersion(File file) {
+        String fileName = file.getName();
+
+        int indexOfSkJson = fileName.indexOf("skjson");
+        int indexOfFallback = fileName.indexOf("fallback");
+        return fileName.substring(indexOfSkJson + "skjson".length() + 1, indexOfFallback - 1);
+    }
+
+    private void fallback() {
+        if (!Api.Records.PLUGIN_FALLBACK_ENABLED) return;
+
+        var f = new File(dataFolder, "libraries");
+
+        if (f.isDirectory()) {
+            var path = f.toPath();
+            try (Stream<Path> stream = Files.list(path)) {
+                stream
+                    .filter(Files::isRegularFile)
+                    .filter(file -> file.toString().endsWith(".fallback"))
+                    .map(Path::toFile)
+                    .forEach(t -> {
+                        try {
+                            SkJson.info("Loading fallback %s", t);
+
+                            String version = getFallbackVersion(t);
+                            String propertiesVersion = fallbackProperties.getProperty("version.minimum.supported");
+
+                            if (!version.equals(propertiesVersion)) {
+                                SkJson.info("Fallback version %s is not supported, skipping", version);
+                                return;
+                            }
+                            containsFallback(t);
+                        } catch (Exception e) {
+                            SkJson.exception(e, "Unable to load fallback %s", t);
+                        }
+                    });
+            } catch (IOException e) {
+                SkJson.exception(e, "Unable to list files in %s", path);
+            }
+        }
+        
+
+        SkJson.info("Fallback properties: %s", fallbackProperties.getProperty("version.minimum.supported"));
+    }
+
+    public boolean isEnabled() {
+        return Api.Records.PLUGIN_FALLBACK_ENABLED;
+    }
+}
---
+++ src/main/java/cz/coffeerequired/fallback/FallbackProperties.java
@@ -0,0 +1,49 @@
+package cz.coffeerequired.fallback;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import cz.coffeerequired.SkJson;
+
+public class FallbackProperties {
+    private static FallbackProperties instance;
+    private final Properties fallback;
+
+    private FallbackProperties() {
+        this.fallback = new Properties();
+        this.loadFallback(new File(SkJson.getInstance().getDataFolder(), "libraries/configuration.properties"));
+    }
+
+    public static FallbackProperties getInstance() {
+        if (instance == null) {
+            instance = new FallbackProperties();
+        }
+        return instance;
+    }
+
+    private void loadFallback(File file) {
+        try {
+            SkJson.debug("Loading fallback properties from %s", file);
+            SkJson.debug("Fallback properties exists: %s", file.exists());
+            this.fallback.load(new FileInputStream(file));
+        } catch (IOException e) {
+            SkJson.severe("Failed to load fallback properties: %s", e.getMessage());
+        }
+    }
+
+    public String getProperty(String key) {
+        return this.fallback.getProperty(key);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getProperty(String key, Class<T> type) {
+        try {
+            return (T) this.fallback.getProperty(key);
+        } catch (ClassCastException e) {
+            SkJson.severe("Failed to cast fallback property: %s", e.getMessage());
+            return null;
+        }
+    }
+}
